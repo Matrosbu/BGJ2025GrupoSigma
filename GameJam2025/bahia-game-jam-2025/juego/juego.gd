c439a6473bd376dab32a1a2617f080939725a584
Matias Rosenzvaig <matrosbu@gmail.com>
2025-11-29 15:33:34 -0300

    Enemigo lanzador

--- GameJam2025/bahia-game-jam-2025/juego/juego.gd
+++ GameJam2025/bahia-game-jam-2025/juego/juego.gd
@@ -4,11 +4,17 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	spawn_enemy()
+	spawn_thrower()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func spawn_enemy():
 	var enemy = preload("res://juego/Enemigo.tscn").instantiate()
-	enemy.global_position = $SpawnPoint1.global_position
+	enemy.global_position = $SpawnEnemigoSimple.global_position
 	add_child(enemy)
+
+func spawn_thrower():
+	var thrower = preload("res://juego/EnemigoLanzador.tscn").instantiate()
+	thrower.global_position = $SpawnThrower.global_position
+	add_child(thrower)

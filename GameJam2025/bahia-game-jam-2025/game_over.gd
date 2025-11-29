2bbf32c9fc9aba0896ad3a7347416561bf83f026
JereTambu <jeretambuuni@gmail.com>
2025-11-29 17:19:00 -0300

    Implementacion pantalla game over

---
+++ GameJam2025/bahia-game-jam-2025/game_over.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_button_reintentar_pressed() -> void:
+	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_button_menu_principal_pressed() -> void:
+	get_tree().change_scene_to_file("res://MenuPrincipal.tscn")

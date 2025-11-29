4896c8479f054ec7d1e80e5d3fe97ef01e28bbaf
Matias Rosenzvaig <matrosbu@gmail.com>
2025-11-29 12:23:38 -0300

    Implementado ataque

--- GameJam2025/bahia-game-jam-2025/juego/character_body_2d.gd
+++ GameJam2025/bahia-game-jam-2025/juego/character_body_2d.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 var speed = 300.0
 var jump_speed = -400.0
+var is_attacking = false
+
 
 # Get the gravity from the project settings so you can sync with rigid body nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -18,5 +20,20 @@ func _physics_process(delta):
 	# Get the input direction.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	velocity.x = direction * speed
+	
+	if Input.is_action_just_pressed("attack") and !is_attacking:
+		is_attacking = true
+		$Sprite.play("attack")
+		$Area2D/AttackHitbox.disabled = false
+		$Area2D/AttackHitbox.visible = true
+		
 
 	move_and_slide()
+
+
+func _on_sprite_animation_finished() -> void:
+	if $Sprite.animation == "attack":
+		$Area2D/AttackHitbox.disabled = true
+		$Area2D/AttackHitbox.visible = false
+		$Sprite.play("standing")
+		is_attacking = false

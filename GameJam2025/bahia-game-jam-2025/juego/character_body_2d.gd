80774a4188a9537a06e54f035a5675adb65a8109
JereTambu <jeretambuuni@gmail.com>
2025-11-29 15:39:01 -0300

    Enemigo muere, cambios varios

--- GameJam2025/bahia-game-jam-2025/juego/character_body_2d.gd
+++ GameJam2025/bahia-game-jam-2025/juego/character_body_2d.gd
@@ -1,8 +1,12 @@
 extends CharacterBody2D
 
+var vida = 100
 var speed = 300.0
 var jump_speed = -400.0
 var is_attacking = false
+var tiempo_iframe = 0.75
+var iframe = false
+var can_attack = false
 
 # Get the gravity from the project settings so you can sync with rigid body nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -24,8 +28,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("attack") and !is_attacking:
 		is_attacking = true
 		$Sprite.play("attack")
-		$Area2D/AttackHitbox.disabled = false
-		$Area2D/AttackHitbox.visible = true
+		$HitboxGolpe/AttackHitbox.visible = true
 		
 
 	move_and_slide()
@@ -33,7 +36,51 @@ func _physics_process(delta):
 
 func _on_sprite_animation_finished() -> void:
 	if $Sprite.animation == "attack":
-		$Area2D/AttackHitbox.disabled = true
-		$Area2D/AttackHitbox.visible = false
+		$HitboxGolpe/AttackHitbox.visible = false
 		$Sprite.play("standing")
 		is_attacking = false
+
+func take_damage(amount):
+	if iframe:
+		return
+	vida -= amount
+	activar_iframe()
+	print("HP Jugador:" , vida)
+
+func activar_iframe():
+	iframe = true
+	
+	var sprite = $Sprite
+	## pestañeo cuando toma hit
+	var blink = get_tree().create_timer(tiempo_iframe,true)
+	while blink.time_left > 0:
+		sprite.modulate.a = 0.3
+		await get_tree().create_timer(0.1).timeout
+		sprite.modulate.a = 1.0
+		await get_tree().create_timer(0.1).timeout
+	iframe = false
+	sprite.modulate.a = 1.0
+	
+	
+func die():
+	pass ##a implementar jeje 
+	
+func _on_hurt_box_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy"):
+		take_damage(10)
+
+
+func _on_sprite_frame_changed() -> void:
+	if $Sprite.animation == "attack":
+		if $Sprite.frame == 2:
+			hit_frame()
+
+func hit_frame():
+	var bodies = $HitboxGolpe.get_overlapping_bodies()
+	for b in bodies:
+		if b.is_in_group("enemy"):
+			can_attack = true
+			if (can_attack):
+				b.take_damage(25)
+				print("Golpeando enemigo")
+				can_attack=false
--- GameJam2025/bahia-game-jam-2025/juego/enemigo.gd
+++ GameJam2025/bahia-game-jam-2025/juego/enemigo.gd
@@ -1,13 +1,20 @@
 extends CharacterBody2D
 
+var hp = 100
 var speed = 80
 var player
 
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
+	add_to_group("enemy")
 
 func _physics_process(delta):
 	if player:
 		var dir = (player.global_position - global_position).normalized()
 		velocity = dir * speed
 		move_and_slide()
+func take_damage(amount): 
+	hp-= amount
+	if hp <= 0:
+		queue_free()
+	print("HP ENEMIGO: ", hp)
